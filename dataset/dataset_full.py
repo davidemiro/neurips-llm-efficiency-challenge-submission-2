e4d244ee01679d9258bfd24c6b4987589a92c164
mirodavide <filomenodavide.miro@gmail.com>
2023-10-15 13:06:22 +0200

    Setup Mistral training dataset

--- dataset/dataset_full.py
+++ dataset/dataset_full.py
@@ -1,5 +1,6 @@
 from datasets import load_dataset,concatenate_datasets
-
+import sys
+sys.path.insert(0, "/content/neurips_llm_efficiency_challenge")
 from dataset_orca import load_orca
 from dataset_p3_multi_qa import load_p3_multi_qa
 from dataset_multi_news_summarize import load_multi_news_summarize
